3dc4efa7f13ce1d83143c7bd6b81825ba4dad331
Georgi Yolovski <bluef0rever@abv.bg>
2013-08-14 11:56:52 +0300

    Added new sample WebApi project.

---
+++ PinkPantherSwagChat/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PinkPantherSwagChat
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
